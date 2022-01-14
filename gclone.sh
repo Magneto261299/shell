9a65d7f8cbc58cc0afd422e3387248a0057e3431
Dr. Metformin <76214556+anymeofu@users.noreply.github.com>
2022-01-14 23:56:24 +0530

    Update gclone.sh

--- gclone.sh
+++ gclone.sh
@@ -1,44 +1,44 @@
 #!/usr/bin/env bash
-
+ 
 set -o errexit
 set -o pipefail
 set -o nounset
-
+ 
 if ! command -v systemctl >/dev/null 2>&1; then
     echo "> Sorry but this scripts is only for Linux with systemd, eg: Ubuntu 16.04+/Centos 7+ ..."
     exit 1
 fi
-
+ 
 if [[ $(id -u) -ne 0 ]]; then
     echo "This script must be run as root" 
     exit 1
 fi
-
+ 
 CLDBIN=/usr/bin/gclone
 OSARCH=$(uname -m)
 case $OSARCH in 
     x86_64)
-        BINTAG=Linux-x86-64
+        BINTAG=linux-amd64
         ;;
     i*86)
-        BINTAG=Linux-i386
+        BINTAG=linux-386
         ;;
     arm64)
-        BINTAG=Linux-arm64
+        BINTAG=linux-arm64
         ;;
     arm*)
-        BINTAG=Linux-armv6
+        BINTAG=linux-arm
         ;;
     *)
         echo "unsupported OSARCH: $OSARCH"
         exit 1
         ;;
 esac
-
-Url=$(wget -qO- https://api.github.com/repos/dogbutcat/gclone/releases/latest \
+TAGNAME=$(wget -qO- https://api.github.com/repos/dogbutcat/gclone/releases/latest | grep tag_name | cut -d '"' -f 4)
+URL=$(wget -qO- https://api.github.com/repos/dogbutcat/gclone/releases/latest \
 | grep browser_download_url | grep "$BINTAG.zip" | cut -d '"' -f 4)
-wget ${url} -O "gclone.zip"
-unzip -j "gclone*zip" "*/gclone" -d "${CLDBIN}"
+wget -nv ${URL}
+unzip -j "gclone-$TAGNAME-$BINTAG.zip" "gclone-$TAGNAME-$BINTAG/gclone" -d "${CLDBIN}" && rm gclone-$TAGNAME-$BINTAG.zip
 chmod 0755 ${CLDBIN}
-
+ 
 gclone version
