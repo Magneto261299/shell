437fce34c1006ebc9529df5314b1642a70280d27
Dr. Metformin <76214556+anymeofu@users.noreply.github.com>
2022-01-14 23:28:33 +0530

    Update gclone.sh

--- gclone.sh
+++ gclone.sh
@@ -38,7 +38,7 @@ esac
 Url=$(wget -qO- https://api.github.com/repos/dogbutcat/gclone/releases/latest \
 | grep browser_download_url | grep "$BINTAG.zip" | cut -d '"' -f 4)
 wget -nv ${url} -O "gclone.zip"
-unzip -jo gclone.zip -d "${CLDBIN}"
+unzip -j "gclone*zip" "*/gclone" -d "${CLDBIN}"
 chmod 0755 ${CLDBIN}
 
 gclone version
