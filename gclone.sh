e67e58b2bd46892790b71a73c8314e855111d3e6
Dr. Metformin <76214556+anymeofu@users.noreply.github.com>
2022-01-15 08:55:09 +0530

    Update gclone.sh

--- gclone.sh
+++ gclone.sh
@@ -2,13 +2,6 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-if ! command -v systemctl >/dev/null 2>&1; then
-    echo "> Sorry but this scripts is only for Linux with systemd, eg: Ubuntu 16.04+/Centos 7+ ..."
-    exit 1
-fi
-if [[ $(id -u) -ne 0 ]]; then
-    echo "This script must be run as root" 
-    exit 1
 fi
 CLDBIN=/usr/bin/gclone
 OSARCH=$(uname -m)
@@ -37,7 +30,6 @@ printenv TAGNAME
 URL=$(wget -qO- https://api.github.com/repos/dogbutcat/gclone/releases/latest \ | grep browser_download_url | grep "$BINTAG.zip" | cut -d '"' -f 4)
 printenv URL
 wget -nv ${URL}
-ls
 unzip -j "gclone-$TAGNAME-$BINTAG.zip" "gclone-$TAGNAME-$BINTAG/gclone" -d "/usr/bin/" && rm gclone-$TAGNAME-$BINTAG.zip
 chmod 0755 ${CLDBIN}
 gclone version
